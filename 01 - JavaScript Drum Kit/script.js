84421b4fa90159d08f65cfd81788a15c27a3849e
theopmw <theowrightdev@gmail.com>
2023-03-16 10:16:20 +0000

    Add logic to register keys a-l on keydown

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,4 @@
 window.addEventListener('keydown', function (e) {
-  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return; // Stop the function from running
 });
