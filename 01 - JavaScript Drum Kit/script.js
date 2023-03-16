c4204ed8b057f2ffb42c568759c1a909fb0d9617
theopmw <theowrightdev@gmail.com>
2023-03-16 10:21:06 +0000

    Rewind audio to start and bind sounds to keydown

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('keydown', function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   if (!audio) return; // Stop the function from running
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
 });
