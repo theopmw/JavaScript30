c704a31f8f45b0ae3b8a981e55e08f2c7c5db9c4
theopmw <theowrightdev@gmail.com>
2024-01-06 09:26:38 +0000

    Get all elements

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,12 @@
+// Get Elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// Build out Functions
+
+// Hook up Event Listeners
